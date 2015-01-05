9228959a8690d58a1f9b542e9ff997c617e34208
Balthazar Rouberol <balthazar.rouberol@ubertas.co.uk>
2015-01-05 09:37:47 +0000

    Project layout

---
+++ README.rst
@@ -0,0 +1,3 @@
+Mashdown will split a Youtube mashup video into individual audio files, with documented audio tags.
+
+Warning: For Mashdown to work, the video must come with a tracklist listing the start time and track name for each track. The tracklist can either be in the Youtube description or submitted manually.
---
+++ setup.py
@@ -0,0 +1,43 @@
+import re
+
+from setuptools import setup
+
+with open('mashdown/__init__.py') as f:
+    version = re.search(
+        r'(?<=__version__ = \')\d\.\d\.\d(?=\')',
+        f.read()
+    ).group()
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(
+    name=u'mashdown',
+    version=version,
+    description=u'Splits a youtube mashup video in a list of tagged audio files',
+    long_description=readme,
+    author=u'Balthazar Rouberol',
+    author_email=u'brouberol@imap.cc',
+    license='License :: OSI Approved :: MIT License',
+    packages=['mashdown'],
+    install_requires=['pydub'],
+    entry_points={
+        'console_scripts': ['mashdown=mashdown.cmd:main']
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: TODO',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Multimedia :: Sound/Audio :: Conversion',
+        'Topic :: Multimedia :: Video :: Conversion'
+    ],
+    zip_safe=False,
+)
