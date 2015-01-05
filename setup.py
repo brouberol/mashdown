43ad157b058693fd38280a802ed4da92301ef4f1
Balthazar Rouberol <balthazar.rouberol@ubertas.co.uk>
2015-01-05 16:55:08 +0000

    Add a proper License PyPI classifier

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Natural Language :: English',
-        'License :: TODO',
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
